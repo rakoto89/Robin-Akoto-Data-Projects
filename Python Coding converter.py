e27a054ee808bc8e44cd19368c622d7b8d14fe18
rakoto89 <121390005+rakoto89@users.noreply.github.com>
2022-12-25 10:58:30 -0500

    Add files via upload

---
+++ Python Coding converter.py
@@ -0,0 +1,8 @@
+dollars = input( "Please input how many dollars you want to convert to euros. " " $ ")
+Euros = int(dollars) * 0.73
+print("€",Euros)
+
+dollars = input("Please input how many dollars you want to convert to yen. " " $ ")
+Yen=int(dollars) * 0.0358
+print("¥",Yen)
+
