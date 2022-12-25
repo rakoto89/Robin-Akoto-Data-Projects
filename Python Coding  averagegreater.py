1cd0a40a2e5299ef044b383312b61c5843bbde7c
rakoto89 <121390005+rakoto89@users.noreply.github.com>
2022-12-25 10:53:44 -0500

    Add files via upload

---
+++ Python Coding  averagegreater.py
@@ -0,0 +1,10 @@
+value_1 = 3
+value_2 = 6
+value_3 = 9
+value_4 = 12
+value_5 = 15
+
+average = (value_1 +value_2+value_3+value_4+value_5)
+
+if average > 20:
+    print("average is greater than 20")
