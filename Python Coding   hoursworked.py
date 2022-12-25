1729be8d13ff781301ca5e7f2c83a49f3c42d5fc
rakoto89 <121390005+rakoto89@users.noreply.github.com>
2022-12-25 10:52:11 -0500

    Add files via upload

---
+++ Python Coding   hoursworked.py
@@ -0,0 +1,9 @@
+hrs = input("Please input how many hours you work during the day. ")
+hours_worked5day = int(hrs) * 5
+
+print(hours_worked5day)
+
+hrs = input("Please input how many hours you work during the day. ")
+hours_worked252day = int(hrs) * 252
+
+print(hours_worked252day)
